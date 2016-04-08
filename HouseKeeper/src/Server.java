11e876d23709ade15d64468cab3b69d00e315124
Karanbir Chahal <karanbleep@gmail.com>
2016-04-08 23:15:59 +0530

    Lambda Expressions, Java 8 FTW

--- HouseKeeper/src/Server.java
+++ HouseKeeper/src/Server.java
@@ -11,16 +11,12 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class Server extends Application implements EventHandler<ActionEvent> {
+public class Server extends Application {
 
-    Button button;
+    Button serverStart;
 
     public static void main(String[] args) {
         launch(args);
-
-       // int port;
-       // port = 9000;
-       // new ServerMain(port);
     }
 
     @Override
@@ -28,22 +24,17 @@ public class Server extends Application implements EventHandler<ActionEvent> {
 
         primaryStage.setTitle("HouseKeeper Server");
 
-        button = new Button();
-        button.setText("Start your server,grl");
-        button.setOnAction(this);
+        serverStart = new Button();
+        serverStart.setText("Start your server,grl");
+        serverStart.setOnAction(e -> new ServerMain(9000));
 
         StackPane layout = new StackPane();
-        layout.getChildren().add(button);
+        layout.getChildren().add(serverStart);
 
         Scene scene = new Scene(layout,300,250);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    @Override
-    public void handle(ActionEvent event) {
-        if(event.getSource() == button) {
-            new ServerMain(9000);
-        }
-    }
+
 }
